1ae0c9f6508ea073bf699af3c7c528d48ba41f3b
linhlam21 <hungluongjuly23.02@gmail.com>
2023-01-27 17:52:44 +0700

    lan thu 2

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -18,8 +18,10 @@ public class MathUtilLive {
         
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual: " + result);
-        long result21 = MathUtil.getFactorial(21);
-        System.out.println("21! = error; actual: " + result21);
+//        long result21 = MathUtil.getFactorial(21);
+//        System.out.println("21! = error; actual: " + result21);
+        long result20 = MathUtil.getFactorial(20);
+        System.out.println("20! = " + result20);
     }
     
 }
